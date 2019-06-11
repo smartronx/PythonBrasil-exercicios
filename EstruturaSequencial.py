d014b90ac69c8e4c8d19b0d10920405b0454ae51
andrecamaro <andre@camaroelectric.com.br>
2019-06-11 13:23:29 -0300

    exercícios 1 a 4

---
+++ EstruturaSequencial.py
@@ -0,0 +1,52 @@
+#1-Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+#Resposta:
+print(' * Resposta exercício 1:')
+
+#Código da solução:
+print('Alo Mundo')
+#------------------------------------------------------------
+
+#2-Faça um Programa que peça um número e então mostre a mensagem 
+#O número informado foi [número].
+
+#Resposta:
+print(' * Resposta exercício 2:')
+
+#Código da solução:
+print('O Número informado foi:  ',int(input('Por favor digite um número inteiro ')))
+
+
+#3-Faça um Programa que peça dois números e imprima a soma.
+
+#Resposta:
+print(' * Resposta exercício 3:')
+
+#Código da solução:
+print('A soma dos dois números informados é = ',int(input('Digite o primeiro número '))+int(input('Digite o segundo Número ')))
+
+#4-Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+#Resposta:
+print(' * Resposta exercício 4:')
+
+#Código da solução:
+print('A Média das notas  é = ',float(input('Digite a primeira nota '))+float(input('Digite a segunda nota '))+float(input('Digite a terceira nota '))+float(input('Digite a quarta nota '))/4)
+
+#ou
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+nota3 = float(input('Digite a terceira nota: '))
+nota4 = float(input('Digite a quarta nota: '))
+
+print ('A media das notas é = ', (nota1 + nota2 + nota3 + nota4)/4)
+
+#5-Faça um Programa que converta metros para centímetros.
+
+#Resposta:
+print(' * Resposta exercício 5:')
+
+#Código da solução:
+
+    
