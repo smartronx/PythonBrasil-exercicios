7de729ce56570e4da3d883195b27dda49ac5396e
andrecamaro <andre@camaroelectric.com.br>
2019-06-11 16:39:41 -0300

    deletado pasta anterior com os exercícios todos juntos

--- 02_MostraNumero.py
+++
@@ -1,9 +0,0 @@
-#2-Faça um Programa que peça um número e então mostre a mensagem 
-#O número informado foi [número].
-
-#Resposta:
-print(' * Resposta exercício 2:')
-
-#Código da solução:
-print('O Número informado foi:  ',int(input('Por favor digite um número inteiro ')))
-#------------------------------------------------------------
